95752d6e54ddcb7e7fcc3a6e93aab6ab9a4f53df
julienBelinga <julienbelinga29@gmail.com>
2021-03-18 22:55:59 +0100

    CardView (not working)

--- app/src/main/java/fr/JulienBelinga/naturecollection/adapter/plantAdapter.kt
+++ app/src/main/java/fr/JulienBelinga/naturecollection/adapter/plantAdapter.kt
@@ -1,6 +1,8 @@
 package fr.JulienBelinga.naturecollection.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import fr.JulienBelinga.naturecollection.R
@@ -10,5 +12,17 @@ class PlantAdapter : RecyclerView.Adapter<PlantAdapter.ViewHolder>(){
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val plantImage = view.findViewById<ImageView>(R.id.image_item)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_plant, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 5
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
 
 }
--- app/src/main/java/fr/JulienBelinga/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/fr/JulienBelinga/naturecollection/fragments/HomeFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.JulienBelinga.naturecollection.R
+import fr.JulienBelinga.naturecollection.adapter.PlantAdapter
 
 class HomeFragment:Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+
+        //récupération du recycler view
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        horizontalRecyclerView.adapter = PlantAdapter()
+
+        return view
     }
 }
