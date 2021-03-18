f37b5d124c94a3b7cfb016c189cdad9e105f4392
julienBelinga <julienbelinga29@gmail.com>
2021-03-18 17:36:40 +0100

    compile
    ça compile
    Kanit-Regular.ttf -> kanit.tff

--- app/src/main/java/fr/JulienBelinga/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/JulienBelinga/naturecollection/MainActivity.kt
@@ -2,11 +2,18 @@ package fr.JulienBelinga.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.JulienBelinga.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit();
     }
 }
